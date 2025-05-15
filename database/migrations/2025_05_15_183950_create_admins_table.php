70ddd5157d5a17ff412672a5ceae67b914066a2d
yashirpal <yashveersoftwere06@gmail.com>
2025-05-16 00:19:41 +0530

    admin migrtion fix for role

--- database/migrations/2025_05_15_183950_create_admins_table.php
+++ database/migrations/2025_05_15_183950_create_admins_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->string('password');
-            $table->string('role');
+            $table->string('role')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
